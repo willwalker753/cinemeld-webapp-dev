51c3dd60238544b3164c54095167437713d6a505
Will Walker <willwalkerdev@gmail.com>
2023-06-04 00:14:21 +0000

    remove github workflow, add cloudflare pages

--- source/src/App.js
+++ source/src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const App = () => {
-    const basename = `/${window.location.pathname.split("/")[1]}/`;
     return (
-        <div>
-            <h1>Hello World !</h1>
-            <Link to={`/about`}>About</Link>
-        </div>
+        <h1>Hello World!</h1>
     )
 }
 
--- source/src/index.js
+++ source/src/index.js
@@ -1,42 +1,12 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter, Routes, Route, useLocation } from "react-router-dom";
-import App from './App';
-import About from './About';
-import './main.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./main.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const basename = `/${window.location.pathname.split("/")[1]}/`;
-
-const Temp = () => {
-    const location = useLocation();
-
-    // useEffect(() => {
-    //     // If the page path is using hash routing
-    //     // Then add a new history entry using standard page routing
-    //     // This runs every time the router changes pages
-    //     if (window?.location?.hash.startsWith(`#${basename}`)) {
-    //         const pagePath = window.location.hash.replace(`#`, '');
-    //         window.history.pushState(null, null, pagePath)
-    //     }
-    //     // if (window?.location?.pathname === `${basename}`) {
-    //     //     window.history.pushState(null, null, `${basename}#${basename}`)
-    //     // }
-    // }, [location]);
-
-    return (
-            <Routes>
-                <Route exact path={`/about`} element={<About />} />
-                <Route exact path={`/`} element={<App />} />
-        </Routes>
-    )
-}
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <HashRouter>
-            <Temp />
-        </HashRouter> 
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
