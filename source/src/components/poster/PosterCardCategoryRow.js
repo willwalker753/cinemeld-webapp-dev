639c9c9f0881d5dc9f63e18ec8577ded73957f27
Will Walker <willwalkerdev@gmail.com>
2024-01-20 23:10:13 +0000

    change carousel from swiper to glide, still facing issues

--- source/src/components/poster/PosterCardCategoryRow.js
+++ source/src/components/poster/PosterCardCategoryRow.js
@@ -1,8 +1,8 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useState, useEffect, useRef } from "react";
 import DetailedPosterCard from "./posterCard/DetailedPosterCard";
 import Button from "../button/Button";
 import { ChevronLeft, ChevronRight } from "@mui/icons-material";
-import { v4 as uuidv4 } from "uuid";
+import Glide from '@glidejs/glide'
 import "./posterCardCategoryRow.css";
 
 const PosterCardCategoryRow = ({
@@ -10,37 +10,67 @@ const PosterCardCategoryRow = ({
     cards=[],    
     ...rest
 }) => {    
-    const swiperContainerId = useMemo(() => `poster-card-category-row-swiper-container-${uuidv4()}`, []);
-    const slideCountPerClick = useMemo(() => 3, []); // the number of slides that are advanced when the next or prev button is clicked
-    const [ prevButtonDisabled, setPrevButtonDisabled ] = useState(true);
-    const [ nextButtonDisabled, setNextButtonDisabled ] = useState(false);
+    const glideRef = useRef(null);
+    // const slideCountPerClick = useMemo(() => 3, []); // the number of slides that are advanced when the next or prev button is clicked
+    // const [ prevButtonDisabled, setPrevButtonDisabled ] = useState(true);
+    // const [ nextButtonDisabled, setNextButtonDisabled ] = useState(false);
 
-    const getSwiper = () => {
-        return document.getElementById(swiperContainerId).swiper;
-    }
+    useEffect(() => {
+        setTimeout(() => {
 
-    const goPrevSlide = () => {
-        const swiper = getSwiper();
-        for (let i=0; i<slideCountPerClick; i++) {
-            swiper.slidePrev();
-        }        
-        afterSlideChange();        
-    }
+            const glide = new Glide(glideRef.current, {
+                type: 'slider',
+                // bound: true, // true = dont show partial view of slides at the end
+                gap: 10, // pixel space between slides
+                swipeThreshold: 80, // pixels of mobile swiping required to change slides
+                dragThreshold: 120, // pixels of desktop swiping required to change slides
+                perTouch: false, // max number of slides to swipe at once
+                touchRatio: 0.7,
+                animationDuration: 200,
+                rewind: false,
+                perView: 7,
+                breakpoints: {
+                    800: {
+                        perView: 3,
+                    },
+                    1200: {
+                        perView: 5,
+                    },
+                }
+            })
+            glide.mount()
+        },500)
 
-    const goNextSlide = () => {
-        const swiper = getSwiper();
-        for (let i=0; i<slideCountPerClick; i++) {
-            swiper.slideNext();
-        }
-        afterSlideChange();        
-    }
+        // return () => {
+        //     glide.destroy();
+        // };
+    }, [])
+    // const getSwiper = () => {
+    //     return document.getElementById(swiperContainerId).swiper;
+    // }
 
-    const afterSlideChange = () => {
-        const swiper = getSwiper();
-        // enable or disable the navigation buttons depending on if there are more slides
-        setPrevButtonDisabled(swiper.isBeginning)
-        setNextButtonDisabled(swiper.isEnd)
-    }
+    // const goPrevSlide = () => {
+    //     const swiper = getSwiper();
+    //     for (let i=0; i<slideCountPerClick; i++) {
+    //         swiper.slidePrev();
+    //     }        
+    //     afterSlideChange();        
+    // }
+
+    // const goNextSlide = () => {
+    //     const swiper = getSwiper();
+    //     for (let i=0; i<slideCountPerClick; i++) {
+    //         swiper.slideNext();
+    //     }
+    //     afterSlideChange();        
+    // }
+
+    // const afterSlideChange = () => {
+    //     const swiper = getSwiper();
+    //     // enable or disable the navigation buttons depending on if there are more slides
+    //     setPrevButtonDisabled(swiper.isBeginning)
+    //     setNextButtonDisabled(swiper.isEnd)
+    // }
 
     return (
         <div className="poster-card-category-row-container" {...rest}>
@@ -50,83 +80,74 @@ const PosterCardCategoryRow = ({
                     <Button 
                         className="poster-card-category-row-nav-button"
                         styleType="surface"
-                        onClick={goPrevSlide} 
-                        disabled={prevButtonDisabled}
+                        // onClick={goPrevSlide} 
+                        // disabled={prevButtonDisabled}
                     >
                         <ChevronLeft sx={{ color: "inherit", fontSize: "inherit" }} /> 
                     </Button>
                     <Button 
                         className="poster-card-category-row-nav-button"
                         styleType="surface"
-                        onClick={goNextSlide}
-                        disabled={nextButtonDisabled}
+                        // onClick={goNextSlide}
+                        // disabled={nextButtonDisabled}
                     >
                         <ChevronRight sx={{ color: "inherit", fontSize: "inherit" }} />
                     </Button>
                 </div>
             </div>
+
+
+            {/* 
+            
+            
+            left off here
+
+            glide doesnt seem to be working out
+
+            maybe try splide
+            https://splidejs.com/
+            see the "drag free" demo
+
+            dont forget to test on mobile before continuing too far
+            remember swiper was jerky/laggy on mobile
+            
+            */}
+
+
+
+
             <div className="poster-card-category-row">
-                <swiper-container
-                    id={swiperContainerId}
-                    slides-per-view="auto"
-
-
-                /*
-
-                    Left off here
-
-                    swiper is slow/jerky on mobile in multiple browsers.
-
-                    i added css-mode below, and that might fix it, need to test
-
-                    if it does then i need to fix other stuff. 
-                        like scrolling on desktop, 
-                        the spacing on the first (and maybe last slide), 
-                        and the number of slides scrolled each time the nav button is clicked
-
-
-                */
-
-                    simulate-touch={true} // Enable click and drag on desktop
-                    // breakpoints={{
-                    //     // Adjust settings based on viewport width
-                    //     768: {
-                    //         slidesPerView: 3, // Number of slides visible on smaller screens
-                    //         simulateTouch: true, // Still enable touch simulation on smaller screens
-                    //     },
-                    //     // Add more breakpoints as needed
-                    // }}
-
-
-
-                    // css-mode="true"
-                    // cssMode={true} // Enable CSS mode for better mobile performance
-                    // navigation={true} // Show navigation buttons
-                    // simulateTouch={true} // Enable click and drag on desktop
-                    // loop="true"
-                    // free-mode="true"
-                    // navigation="true"
+                <div 
+                    ref={glideRef} 
+                    className="poster-card-category-row-carousel"
                 >
-                    {cards.map((card, index) => {
-                        const { title, genreList, votePercent, voteFactor, imageUrl, onClick } = card;                
-                        return (
-                            <swiper-slide
-                                key={index}
-                                lazy="true"
-                            >
-                                <DetailedPosterCard
-                                    key={index}
-                                    title={title}
-                                    genreList={genreList}
-                                    votePercent={votePercent}
-                                    voteFactor={voteFactor}
-                                    imageUrl={imageUrl}
-                                    onClick={onClick}
-                                />
-                            </swiper-slide>
-                        )
-                    })}
-                </swiper-container>
+                    <div 
+                        className="glide__track" 
+                        data-glide-el="track"
+                    >
+                        <ul className="poster-card-category-row-carousel-slides glide__slides">
+                            {cards.map((card, index) => {
+                                const { title, genreList, votePercent, voteFactor, imageUrl, onClick } = card;                
+                                return (
+                                    <li 
+                                        key={index}
+                                        className="glide__slide"
+                                    >
+                                        <DetailedPosterCard
+                                            key={index}
+                                            title={title}
+                                            genreList={genreList}
+                                            votePercent={votePercent}
+                                            voteFactor={voteFactor}
+                                            imageUrl={imageUrl}
+                                            onClick={onClick}
+                                        />
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- source/src/index.js
+++ source/src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import PageRouter from "./PageRouter";
-import { register } from "swiper/element/bundle";
 import { Provider } from "react-redux"
 import store from "./redux/store"
 import "./main.css";
-
-// register Swiper custom elements
-register();
+import "@glidejs/glide/dist/css/glide.core.min.css";
+import "@glidejs/glide/dist/css/glide.theme.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
